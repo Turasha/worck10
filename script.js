8e02ff8ac0025fd582452908a836166a5d4a96b7
Turasha <teonagiox.t@gmail.com>
2024-04-14 19:21:33 +0400

    done

---
+++ script.js
@@ -0,0 +1,147 @@
+// //new promice
+
+// let lottery = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         if (Math.random() >= 0.5) {
+//             resolve("you win");
+//           } else {
+//             reject("you lost");
+//           }
+//     },  3000);
+
+// });
+
+// lottery
+//   .then((Response) => console.log(Response))
+//   .catch((error) => console.log(error));
+
+//   //აქ გვაქვს ფუნქცია სადაც ვაბრუნებინებტ პირობას
+
+//   let wait=function(second){
+//     return new Promise(function(resolve){
+//         setTimeout(resolve, second * 1000)
+//     })
+//   }
+
+//   wait(3)
+//   .then(()=>{
+//     console.log('3 second later');
+//     return wait(2)
+//   }).then(()=>{
+//     console.log('2second later');
+//   })
+
+//practice
+// let imgContainer = document.querySelector(".images");
+// let currentImage;
+// let api =document.getElementById('api')
+
+// let wait = function (second) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, second * 1000);
+//   });
+// };
+
+// let createImgEelement = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     let imgNewEl = document.createElement("img");
+//     imgNewEl.src = imgPath;
+
+//     imgNewEl.addEventListener("load", function () {
+//       imgContainer.appendChild(imgNewEl);
+//       resolve(imgNewEl);
+//     });
+
+//     imgNewEl.addEventListener("error", function () {
+//       reject(new error("img not found"));
+//     });
+//   });
+// };
+// createImgEelement("images/img1.jpg")
+//   .then((image) => {
+//     currentImage=image
+//     console.log("image load");
+//     return wait(2)
+//   })
+//   .then(()=>{
+//     currentImage.style.display='none'
+//     return createImgEelement('images/img2.jpeg')
+//   })
+//   .then((image)=>{
+//     currentImage=image
+//     console.log();
+//     return wait(2)
+//   })
+//   .then((image)=>{
+//     currentImage.style.display='none'
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+//  async  await
+
+//   let input=document.getElementById('input-filter')
+//   let ulElement=document.getElementById('result')
+//   let listItems=[]
+//   function test1(){
+//     setTimeout(() => {
+//         console.log('hello1');
+//     }, 3000);
+//   }
+//   function test2(){
+//     setTimeout(() => {
+//         console.log('hello1');
+//     }, 2000);
+//   }
+
+//   async function testfnc(){
+//     await test1()
+//     await test2()
+//   }
+
+// testfnc();
+
+//  async  await with fetch
+
+let input = document.getElementById("input-filter");
+let ulElement = document.getElementById("result");
+let listItems = [];
+
+async function asyncfnc() {
+  let response = await fetch("https://reqres.in/api/users?page=2");
+  console.log(response);
+  if (!response.ok) {
+    throw new Error("cent fatch data");
+  }
+  let responsedata = await response.json();
+  responsedata.data.forEach((element) => {
+    let li = document.createElement("li");
+    li.textContent = `${element.first_name} ${element.last_name}`;
+    listItems.push(li);
+    ulElement.appendChild(li);
+  });
+}
+
+asyncfnc();
+
+function filtreData(searchItem) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchItem.toLowerCase())) {
+      item.classList.remove("active");
+    } else {
+      item.classList.add("active");
+    }
+  });
+}
+
+input.addEventListener("keyup", function () {
+  filtreData(this.value);
+});
+
+// .then((mosuliInfo)=>{
+//     let p =document.createElement('p')
+//   p. innerText =mosuliInfo.data[1].first_name
+//     api.appendChild(p)
+
+// }).catch((error)=>console.log(error))
